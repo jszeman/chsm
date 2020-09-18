0ad59c39d7c84f3d2c4788973fa1445791b148a7
jszeman <jns.szeman@gmail.com>
2020-09-18 22:21:54 +0200

    Make cqueue double ended
    Add put_left and get_right methods to the cqueue module. This
    will make it possible to use a second queue in the HSM to store
    deferred events and later restore them into the main queue in
    the same order as they arrived.

--- crf/test/tsrc/cqueue_test.c
+++ crf/test/tsrc/cqueue_test.c
@@ -116,6 +116,31 @@ TEST(eq, put_more_than_capacity)
 }
 
 /**
+ * Check that we can get an element even if the queue is full
+ */
+TEST(eq, get_all)
+{
+	cevent_tst  e[4];
+	const cevent_tst  *ep[4];
+	int32_t r;
+
+	cqueue_init(&eq, (const cevent_tst  **)events, 4);
+	for (uint16_t i=0; i<4; i++)
+	{
+		r = eq.put(&eq, &e[i]);
+	}
+
+	for (uint16_t i=0; i<4; i++)
+	{
+		ep[i] = eq.get(&eq);
+
+		TEST_ASSERT(ep[i] == &e[i]);
+	}
+
+	
+}
+
+/**
  * Check that put will roll over, when more events were put/get from
  * the queue.
  */
@@ -154,6 +179,104 @@ TEST(eq, test_get_roll)
 	TEST_ASSERT(&e1 == ep1);
 }
 
+/**
+ * Check that put_left will allow getting the pointer back.
+ */
+TEST(eq, put_left_1_get_1)
+{
+	cevent_tst  e;
+	const cevent_tst  *ep;
+	int32_t r;
+
+	cqueue_init(&eq, (const cevent_tst  **)events, 4);
+	r = eq.put_left(&eq, &e);
+	ep = eq.get(&eq);
+
+	TEST_ASSERT_EQUAL(0, r);
+	TEST_ASSERT(ep == &e);
+}
+
+/**
+ * Check that put_left will result in a stack like operation,
+ * when the items are read back in reverse order.
+ */
+TEST(eq, put_left_2_get_2)
+{
+	cevent_tst  e1, e2;
+	const cevent_tst  *ep1, *ep2;
+	int32_t r1, r2;
+
+	cqueue_init(&eq, (const cevent_tst  **)events, 4);
+	r1 = eq.put_left(&eq, &e1);
+	r2 = eq.put_left(&eq, &e2);
+	ep1 = eq.get(&eq);
+	ep2 = eq.get(&eq);
+
+	TEST_ASSERT_EQUAL(0, r1);
+	TEST_ASSERT_EQUAL(0, r2);
+	TEST_ASSERT(ep1 == &e2);
+	TEST_ASSERT(ep2 == &e1);
+}
+
+/**
+ * Check that put_left is able to insert an element in
+ * front of the previously put in items.
+ */
+TEST(eq, put_left_after_put)
+{
+	cevent_tst  e1, e2, e3;
+	const cevent_tst  *ep1;
+	int32_t r1, r2, r3;
+
+	cqueue_init(&eq, (const cevent_tst  **)events, 4);
+	r1 = eq.put(&eq, &e1);
+	r2 = eq.put(&eq, &e2);
+	r3 = eq.put_left(&eq, &e3);
+	ep1 = eq.get(&eq);
+
+	TEST_ASSERT_EQUAL(0, r1);
+	TEST_ASSERT_EQUAL(0, r2);
+	TEST_ASSERT_EQUAL(0, r3);
+	TEST_ASSERT(ep1 == &e3);
+}
+
+/**
+ * Check that put_left returns -1 when there are no more slots.
+ */
+TEST(eq, put_left_more_than_capacity)
+{
+	cevent_tst  e1;
+	int32_t r;
+
+	cqueue_init(&eq, (const cevent_tst  **)events, 4);
+	for (uint16_t i=0; i<5; i++)
+	{
+		r = eq.put_left(&eq, &e1);
+	}
+
+	TEST_ASSERT(-1 == r);
+}
+
+/**
+ * Test that we can get back the last event from the queue,
+ * when 2 was put in
+ */
+TEST(eq, put_2_get_right_1)
+{
+	cevent_tst  e1, e2;
+	const cevent_tst  *ep;
+	int32_t r1, r2;
+
+	cqueue_init(&eq, (const cevent_tst  **)events, 4);
+	r1 = eq.put(&eq, &e1);
+	r2 = eq.put(&eq, &e2);
+	ep = eq.get_right(&eq);
+
+	TEST_ASSERT_EQUAL(0, r1);
+	TEST_ASSERT_EQUAL(0, r2);
+	TEST_ASSERT(ep == &e2);
+}
+
 
 TEST_GROUP_RUNNER(eq)
 {
@@ -164,12 +287,12 @@ TEST_GROUP_RUNNER(eq)
 	RUN_TEST_CASE(eq, put_more_than_capacity);
 	RUN_TEST_CASE(eq, test_put_roll);
 	RUN_TEST_CASE(eq, test_get_roll);
-//	RUN_TEST_CASE(eq, new_1);
-//	RUN_TEST_CASE(eq, new_1);
-//	RUN_TEST_CASE(eq, new_1);
-//	RUN_TEST_CASE(eq, new_1);
-//	RUN_TEST_CASE(eq, new_1);
-//	RUN_TEST_CASE(eq, new_1);
+	RUN_TEST_CASE(eq, put_left_1_get_1);
+	RUN_TEST_CASE(eq, put_left_2_get_2);
+	RUN_TEST_CASE(eq, put_left_after_put);
+	RUN_TEST_CASE(eq, put_left_more_than_capacity);
+	RUN_TEST_CASE(eq, get_all);
+	RUN_TEST_CASE(eq, put_2_get_right_1);
 //	RUN_TEST_CASE(eq, new_1);
 //	RUN_TEST_CASE(eq, new_1);
 //	RUN_TEST_CASE(eq, new_1);
