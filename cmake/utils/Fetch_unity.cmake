cd2f2611bf7860f3531c335eec4261246dea4cb7
Laszlo Ivanyi <laszlo.ivanyi@mediso.com>
2021-06-02 09:34:55 +0200

    Updated modul tests cmake files

--- cmake/utils/Fetch_unity.cmake
+++ cmake/utils/Fetch_unity.cmake
@@ -2,12 +2,26 @@ include(FetchContent)
 
 set(FETCHCONTENT_QUIET OFF)
 
-FetchContent_Declare(
-  Unity
-  GIT_REPOSITORY  https://github.com/ThrowTheSwitch/Unity.git
-  GIT_TAG         origin/master
-  SOURCE_DIR      ${CMAKE_CURRENT_SOURCE_DIR}/../../deps/unity
-  )
+get_filename_component(DIR ${CMAKE_CURRENT_SOURCE_DIR} DIRECTORY)
+get_filename_component(PARENT_DIR ${DIR} DIRECTORY)
+get_filename_component(MODULE_NAME ${PARENT_DIR} NAME)
+message("MODULE_NAME: -> ${MODULE_NAME}")
+
+if(MODULE_NAME STREQUAL "crf")
+  FetchContent_Declare(
+    Unity
+    GIT_REPOSITORY  https://github.com/ThrowTheSwitch/Unity.git
+    GIT_TAG         origin/master
+    SOURCE_DIR      ${CMAKE_CURRENT_SOURCE_DIR}/../../deps/unity
+    )
+else()
+  FetchContent_Declare(
+    Unity
+    GIT_REPOSITORY  https://github.com/ThrowTheSwitch/Unity.git
+    GIT_TAG         origin/master
+    SOURCE_DIR      ${CMAKE_CURRENT_SOURCE_DIR}/../../../deps/unity
+    )
+endif()
 
 set(UNITY_EXTENSION_FIXTURE ON CACHE INTERNAL "Set UNITY_EXTENSION_FIXTURE CACHE INTERNAL.")
 set(UNITY_EXTENSION_MEMORY ON CACHE INTERNAL "Set UNITY_EXTENSION_MEMORY CACHE INTERNAL.")
