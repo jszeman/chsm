8238e6f24105b30a4d13bd182f3c71129ba97055
Laszlo Ivanyi <laszlo.ivanyi@mediso.com>
2022-03-23 08:09:48 +0100

    Try to fix signal_classes_h dependency in ltc4271_test, spoiler not work yet

--- cmake/utils/set_signal_classes.cmake
+++
@@ -1,6 +0,0 @@
-
-set(CHSM_SIGNAL_CLASSES_H \"${CMAKE_CURRENT_SOURCE_DIR}/inc/signal_classes.h\" CACHE INTERNAL "Set CHSM_SIGNAL_CLASSES_H with local path.") 
-# function(hello param)
-    message("Hello ${CHSM_SIGNAL_CLASSES_H}")    
-# endfunction(hello param)
-
